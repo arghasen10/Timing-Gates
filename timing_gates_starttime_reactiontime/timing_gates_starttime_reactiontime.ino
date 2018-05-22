6e5caf596de3dba22bef7a27dc7ee9cae117d13b
ARGHA SEN <arghasen10@gmail.com>
2018-05-22 19:46:11 +0530

    Add files via upload
    Code for Start time(with button) and reaction time(with IR sensors)

---
+++ timing_gates_starttime_reactiontime/timing_gates_starttime_reactiontime.ino
@@ -0,0 +1,36 @@
+#define IR A5
+#define button 9
+int previousVal = 0;
+
+int currentVal;
+int flag1=0;
+int flag2=0;
+int starttime;
+int reactiontime;
+void setup() {
+Serial.begin(9600);
+pinMode(IR,INPUT);
+ pinMode(button,INPUT);
+}
+
+void loop() {
+  if(digitalRead(button)==HIGH && flag1==0){
+    starttime=millis();
+    //Send data to server
+    flag1=1;
+    Serial.print(starttime+1137);
+  }
+ 
+  currentVal=analogRead(IR);
+  if(currentVal>950 && previousVal<currentVal ){
+    
+    previousVal=currentVal;
+    if(previousVal>960 && flag2==0){
+      reactiontime = millis();
+      //Sending data to the server
+      previousVal=0;
+   
+  } }
+
+}
+
